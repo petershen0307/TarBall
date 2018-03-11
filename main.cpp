096d211670c036b76a5da0cea07bca4fae375dff
PeterShen <petershen0307@hotmail.com>
2018-03-11 22:25:45 +0800

    input sstream to Tar class then output to fstream

--- main.cpp
+++ main.cpp
@@ -6,31 +6,24 @@
 
 int main(int argc, char** argv)
 {
-    std::stringstream sstream;
-    sstream.write("hello\0world", 12);
-    std::string str = sstream.str();
-    str.size();
     /* open file for writing */
+
+    /* create the tar file */
+    std::stringstream sstream;
+    LibTar::Tar tarball(sstream);
+    tarball.putFile("C:/Users/PC/Desktop/code/vs_main/tar/x64/Debug/item1.txt", "item1.txt");
+    tarball.putFile("C:/Users/PC/Desktop/code/vs_main/tar/x64/Debug/item2.txt", "item2.txt");
+    /* finalize the tar file */
+    tarball.finish();
+    /* close the file */
     std::fstream out("archive5.tar", std::ios::out | std::ios::binary);
     if (!out.is_open())
     {
         std::cerr << "Cannot open out" << std::endl;
         return EXIT_FAILURE;
     }
-    /* create the tar file */
-    LibTar::Tar tarball(out);
-    /* add item 1 */
-    //tarball.put("item1.txt", "Hello World 1\n");
-    /* add item 2 */
-    //tarball.put("item2.txt", "Hello World 2\n");
-    /* add a file */
-    //tarball.putFile("D:/code/TestSLN/x64/Debug/tarFiles.ilk", "tarFiles.ilk");
-    //tarball.putFile("tarFiles.pdb", "tarFiles.pdb");
-    tarball.putFile("C:/Users/PC/Desktop/code/vs_main/tar/x64/Debug/item1.txt", "item1.txt");
-    tarball.putFile("C:/Users/PC/Desktop/code/vs_main/tar/x64/Debug/item2.txt", "item2.txt");
-    /* finalize the tar file */
-    tarball.finish();
-    /* close the file */
+    const std::string& content = sstream.str();
+    out.write(content.c_str(), content.size());
     out.close();
     /* we're done */
     return EXIT_SUCCESS;;
